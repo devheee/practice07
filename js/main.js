a83f58ee73d11c9661a1169087eb3ca86867ac41
SOHEE <118507151+devheee@users.noreply.github.com>
2023-01-04 17:49:17 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,7 +20,17 @@ $(function () {
         $('.animal_slide').slick('slickNext')
     });
 
+    $('.review_slide').slick({
+        slidesToShow: 3,
+        arrows: false,
+    });
 
+    $('.mainReview .review_arrow i:first-child').on('click', function () {
+        $('.review_slide').slick('slickPrev')
+    });
+    $('.mainReview .review_arrow i:last-child').on('click', function () {
+        $('.review_slide').slick('slickNext')
+    });
 
 
 
